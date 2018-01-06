d70f9af5e8947f060f152318e76e475f56af458b
Diky Diwo Suwanto <dikydiwo@outlook.com>
2018-01-06 23:57:05 +0700

    Super Easy

---
+++ UVA OJ/CP3/Introduction/Getting Started The Easy Problems/12372 - Packing for Holiday.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t,l,w,h;
+
+    scanf("%d", &t);
+    for(int i=1; i<=t; i++){
+        scanf("%d %d %d", &l, &w, &h);
+        if(l > 20 || w > 20 || h > 20) printf("Case %d: bad\n", i);
+        else printf("Case %d: good\n", i);
+    }
+
+    return 0;
+}
